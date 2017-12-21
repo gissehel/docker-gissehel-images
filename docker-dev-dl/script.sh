a3bebf682ba9619e05765bf861c2d8f16278e470
gissehel <public-devgit-dantus@gissehel.org>
2017-12-21 19:46:28 +0100

    Add a new line in history

--- docker-dev-dl/script.sh
+++ docker-dev-dl/script.sh
@@ -14,14 +14,14 @@ DEV=$HOME_USER/dev
 WORKDIR=$DEV/workdir
 TEMPDIR=$DEV/tmp
 
-su -l "$LOGIN" bash -c "mkdir -p $DEV && mkdir -p $WORKDIR && cd $DEV && git clone https://github.com/mcrapet/plowshare/ plowshare4 && sed -i -e 's/umask 0066/umask 0022/' plowshare4/src/download.sh && echo 'export TEMP_DIR=$TEMPDIR' >> $HOME_USER/.bashrc && echo 'cd $WORKDIR' >> $HOME_USER/.bashrc"
+su -l "$LOGIN" bash -c "mkdir -p $DEV && mkdir -p $WORKDIR && cd $DEV && git clone https://github.com/mcrapet/plowshare/ plowshare4 && sed -i -e 's/umask 0066/umask 0022/' plowshare4/src/download.sh && echo 'export TEMP_DIR=$TEMPDIR' >> $HOME_USER/.bashrc && echo 'cd $WORKDIR' >> $HOME_USER/.bashrc && echo \"alias monip=\\\"curl -s http://monip.org | perl -ape 's/<[^>]+>/ /mg; s/ +/ /mg'| grep \\\\\\\" IP \\\\\\\" --color=never | perl -ape 's{ IP : (\S+) (\S+) (.*)}{\\\\\\\$1\\n\\\\\\\$2\\n\\\\\\\$3}'\\\"\" >> $HOME_USER/.bashrc"
 
 cd $DEV/plowshare4
 make install
 su -l "$LOGIN" bash -c "/usr/bin/plowmod --install"
 BASH_HISTORY="$HOME_USER/.bash_history"
 echo "curl http://monip.org" >> "${BASH_HISTORY}"
-echo "curl -s http://monip.org | perl -ape 's/<[^>]+>/ /mg; s/ +/ /mg'| grep \" IP \" --color=never | perl -ape 's{ IP : (\S+) (\S+) (.*)}{\$1\n\$2\n\$3}'" >> "${BASH_HISTORY}"
+echo "monip" >> "${BASH_HISTORY}"
 chown gissehel:gissehel "${BASH_HISTORY}"
 chmod 0644 "${BASH_HISTORY}"
 
