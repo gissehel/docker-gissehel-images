b8f18eb1d21c3f35f6cc845d093a2eaa3141228a
gissehel <public-devgit-frinan@gissehel.org>
2020-05-11 09:40:38 +0000

    Use openjdk instead of oraclejdk for dev-lang-java

--- dockerfiles/local/dev-lang-java/script.sh
+++ dockerfiles/local/dev-lang-java/script.sh
@@ -3,12 +3,9 @@
 #:! /bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
-add-apt-repository -y ppa:webupd8team/java
 apt -y update
-apt -y install oracle-java8-set-default
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-apt -y install oracle-java8-installer && apt-get clean
-update-alternatives --display java
+apt -y install openjdk-8-jdk
+apt-get clean
 
 rm -rf /var/lib/apt/lists/*
 rm -f /var/log/dpkg.log
