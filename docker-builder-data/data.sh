0de1ede46b6cfa2fed6596b26fd1d8f014ae796a
gissehel <public-devgit-frinan@gissehel.org>
2021-04-03 10:03:14 +0000

    Use schedule parameters

--- docker-builder-data/data.sh
+++ docker-builder-data/data.sh
@@ -2,9 +2,11 @@ github_project="gissehel/docker-gissehel-images"
 gitlab_project="registry.gitlab.com/gissehel/docker-gissehel-images"
 dockerhub_prefix="gissehel"
 ghcr_prefix="ghcr.io\/gissehel"
+ghcr_schedule="58 1 * * 5"
 project_url="https://github.com/${github_project}"
 vendor="gissehel"
 src_dir="src"
 project_images="ubuntu-sshd dev dev-lang dev-lang-java dev-dl irssi rtorrent ubuntu-base squid squid-open gollum"
+
 
 
